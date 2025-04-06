77a5d117cd0c3f471141dd11803b438e01d06b4e
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-06 10:36:49 +1000

    Investigate more on distances

--- scripts/FACs/06_comp_distances_clust_wise.R
+++ scripts/FACs/06_comp_distances_clust_wise.R
@@ -131,3 +131,50 @@ dist_df_clust6 |>
   theme(aspect.ratio = 1)
 
 ## Between clusters
+
+### Between 0 and 2
+
+dist_df_clust02 <- dist_df |>
+  filter(from_cluster == 0) |>
+  filter(to_cluster == 2)
+
+dist_df_clust02 |>
+  ggplot(aes(x = dist_highd,
+             y = dist_2d_tsne_30)) +
+  geom_point(alpha=0.5) +
+  theme(aspect.ratio = 1)
+
+dist_df_clust02 |>
+  ggplot(aes(x = dist_highd,
+             y = dist_2d_tsne_15)) +
+  geom_point(alpha=0.5) +
+  theme(aspect.ratio = 1)
+
+###
+dist_df_clust02 <- dist_df |>
+  filter(from_cluster == 2) |>
+  filter(to_cluster == 0)
+
+dist_df_clust02 |>
+  ggplot(aes(x = dist_highd,
+             y = dist_2d_tsne_30)) +
+  geom_point(alpha=0.5) +
+  theme(aspect.ratio = 1)
+
+dist_df_clust02 |>
+  ggplot(aes(x = dist_highd,
+             y = dist_2d_tsne_15)) +
+  geom_point(alpha=0.5) +
+  theme(aspect.ratio = 1)
+
+dist_df_clust02 |>
+  ggplot(aes(x = dist_highd,
+             y = dist_2d_tsne_30)) +
+  geom_point(alpha=0.1) +
+  geom_point(aes(x = dist_highd,
+                 y = dist_2d_tsne_15),
+             alpha=0.1,
+             color = "#8da0cb") +
+  theme(aspect.ratio = 1)
+
+## Add interactivity with 2D layouts, distances comp, highd data
