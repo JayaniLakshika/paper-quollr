4514e9687fc45a18a6e971a94836a355d2ad6c19
JayaniLakshika <jayanilakshika76@gmail.com>
2025-05-19 18:42:16 +1000

    Fixed the references issue for HTML and PDF outputs

--- paper-quollr.R
+++ paper-quollr.R
@@ -263,7 +263,7 @@ scurve_umap_model_vis_n <- langevitour::langevitour(df_exe[1:(length(df_exe)-1)]
 # htmltools::browsable(
 #   tagList(
 #     tags$div(
-#       id = "fig-algo",  # This is the anchor
+#       id = "fig-overview",  # This is the anchor
 #       crosstalk::bscols(
 #         htmltools::div(
 #           style = "display: grid; grid-template-columns: 1fr 1fr;",
@@ -407,7 +407,7 @@ scurve_proj_umap_model4 <- plot_proj(
   interior_annotation(label = "a4", cex = 1) 
 
 
-## ----eval=knitr::is_latex_output(), fig.cap="algorithm", fig.pos='H'----------
+## ----overview, eval=knitr::is_latex_output(), fig.cap="algorithm", fig.pos='H'----
 hex_grid_scurve + wrap_plots(
   scurve_proj_umap_model1, scurve_proj_umap_model2,
   scurve_proj_umap_model3, scurve_proj_umap_model4, 
@@ -438,7 +438,7 @@ hex_grid_scurve + wrap_plots(
 # )
 
 
-## ----eval=knitr::is_latex_output(), out.width="100%", fig.height=5, fig.width=25, fig.pos='H', fig.cap="Key steps for constructing the model on the UMAP layout: (a) NLDR data, (b) hexagon bins, (c) bin centroids, (d) triangulated centroids, and (e) lifting the model into high dimensions. The `Scurve` data is shown."----
+## ----algo-steps, eval=knitr::is_latex_output(), out.width="100%", fig.height=5, fig.width=25, fig.pos='H', fig.cap="Key steps for constructing the model on the UMAP layout: (a) NLDR data, (b) hexagon bins, (c) bin centroids, (d) triangulated centroids, and (e) lifting the model into high dimensions. The `Scurve` data is shown."----
 scurve_umap_plt + hex_grid_poly_scurve +
   hex_centroids_scurve + wireframe_scurve + 
   scurve_proj_umap_model1 +
